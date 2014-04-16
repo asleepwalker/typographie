f378c6cb30983e24c43a4aac88b8d2e0497a231a
asleepwalker <mail@asleepwalker.ru>
2014-04-16 04:17:37 +0300

    Some info in executive files

--- src/engine/main.php
+++ src/engine/main.php
@@ -1,5 +1,14 @@
 <?php
 
+	/*
+		Typographie, v1.0
+		https://github.com/asleepwalker/typographie
+
+		by Artyom "Sleepwalker" Fedosov, 2014
+		http://me.asleepwalker.ru/
+		mail@asleepwalker.ru
+	*/
+
 	$raw = $_POST['raw'];
 	require_once('../typographie.php');
 	$engine = new typographie('plain', 'editor');

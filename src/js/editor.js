198c9b4c43236cebb9628388088f6b994e45aebd
asleepwalker <mail@asleepwalker.ru>
2014-05-02 15:13:58 +0300

    Blackzones now work

--- src/js/editor.js
+++ src/js/editor.js
@@ -60,7 +60,10 @@ App.actions.remove = function(action) {
 		App.actions.list.splice(App.actions.list.indexOf(action), 1);
 };
 
+/* Submit button */
 document.getElementById('submit').onclick = App.send;
+
+/* Input mode change */
 var input_modes = document.getElementById('input_mode').getElementsByTagName('li');
 for (var i = 0; i < input_modes.length; i++) {
 	input_modes[i].onclick = function() {
@@ -71,6 +74,8 @@ for (var i = 0; i < input_modes.length; i++) {
 		}
 	};
 }
+
+/* Output mode change */
 var output_modes = document.getElementById('output_mode').getElementsByTagName('li');
 for (var i = 0; i < output_modes.length; i++) {
 	output_modes[i].onclick = function() {
@@ -81,6 +86,8 @@ for (var i = 0; i < output_modes.length; i++) {
 		}
 	};
 }
+
+/* Output editor turning on/off */
 document.getElementById('editor_checkbox').onclick = function() {
 	if (window.App.editor == 'enabled') {
 		document.getElementById('ec_input').className = 'checkbox';
@@ -90,6 +97,8 @@ document.getElementById('editor_checkbox').onclick = function() {
 		window.App.editor = 'enabled';
 	}
 };
+
+/* Showing options dialog */
 document.getElementById('show_options').onclick = function() {
 	var el = document.getElementById('dialog_wrapper');
 	el.style.display = 'block';
@@ -101,6 +110,11 @@ document.getElementById('show_options').onclick = function() {
 		last = +new Date();
 		if (+el.style.opacity < 1) {
 			(window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 10)
+		} else {
+			var blackzones = document.getElementsByClassName('blackzone');
+			for (var i = 0; i < blackzones.length; i++) {
+				blackzones[i].style.display = 'block';
+			}
 		}
 	};
 
@@ -119,6 +133,32 @@ function resizeDialog() {
 	document.getElementById('bz_right').style.left = bounds.right+'px';
 	document.getElementById('bz_bottom').style.top = bounds.bottom+'px';
 }
+
+/* Hiding options dialog */
+var blackzones = document.getElementsByClassName('bz');
+for (var i = 0; i < blackzones.length; i++) {
+	blackzones[i].onclick = function() {
+		var el = document.getElementById('dialog_wrapper');
+		var last = +new Date();
+		var tick = function() {
+			el.style.opacity = +el.style.opacity - (new Date() - last) / 400;
+			last = +new Date();
+
+			if (+el.style.opacity > 0) {
+				(window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+			} else {
+				el.style.display = 'none';
+				var blackzones = document.getElementsByClassName('blackzone');
+				for (var i = 0; i < blackzones.length; i++) {
+					blackzones[i].style.display = 'none';
+				}
+			}
+		};
+		tick();
+	};
+}
+
+/* Turning on/off options */
 var options = document.getElementById('options').getElementsByTagName('li');
 for (var i = 0; i < options.length; i++) {
 	options[i].getElementsByTagName('div')[0].onclick = function() {

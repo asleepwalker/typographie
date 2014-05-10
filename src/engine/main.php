6d24bc934be7ba655298c2c9854100c02346f150
asleepwalker <mail@asleepwalker.ru>
2014-05-10 21:45:20 +0300

    Highlight

--- src/engine/main.php
+++ src/engine/main.php
@@ -21,11 +21,8 @@
 
 	if ($_POST['highlight'] == 'enabled') {
 		require_once('finediff.class.php');
-		$in = mb_convert_encoding($_POST['raw'], 'HTML-ENTITIES', 'UTF-8');
-		$out = mb_convert_encoding($result, 'HTML-ENTITIES', 'UTF-8');
-		$opcodes = FineDiff::getDiffOpcodes($in, $out);
-		$pre = FineDiff::renderDiffToHTMLFromOpcodes($in, $opcodes);
-		$result = mb_convert_encoding($pre, 'UTF-8', 'HTML-ENTITIES');
+		$opcodes = FineDiff::getDiffOpcodes($_POST['raw'], $result);
+		$result = FineDiff::renderDiffToHTMLFromOpcodes($_POST['raw'], $opcodes);
 		$result = preg_replace('/<del>.*?<\/del>(.*?)<ins>(.*?)<\/ins>/u', '<span class="fix">$1$2</span>', $result);
 		$result = preg_replace('/<del>(.*?)<\/del>/u', '', $result);
 		$result = preg_replace('/<ins>(.*?)<\/ins>/u', '<span class="fix">$1</span>', $result);

f1bd0176169065ada7df36d3aea114badb38c123
asleepwalker <mail@asleepwalker.ru>
2014-05-26 07:31:42 +0300

    Added links in header

--- src/pages/header.php
+++ src/pages/header.php
@@ -7,6 +7,10 @@
 	</head>
 	<body>
 		<header>
+			<nav><ul>
+				<li><a href="?specials">Таблица спецсимволов</a></li>
+				<li><a href="?contacts">Связаться с автором</a></li>
+			</ul></nav>
 			<div id="logo">&laquo;<a href="/">Typographie</a>&raquo;&nbsp;&mdash; подготовка текстов к веб-публикации онлайн</div>
 		</header>
 		<div id="wrap">

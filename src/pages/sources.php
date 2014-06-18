bf149a42252a57c956a37e29f3efe1d3067877ad
asleepwalker <mail@asleepwalker.ru>
2014-06-18 17:52:00 +0300

    Updated info about structure of the repository

--- src/pages/sources.php
+++ src/pages/sources.php
@@ -5,9 +5,12 @@
 			<p>Сервис <b>«Typographie»</b> — это open-source проект, то есть проект с открытыми исходными кодами.</p>
 			<p>Любой человек может принять участие в разработке и улучшении проекта, или же воспользоваться исходными кодами в своих целях. Смотрите <a href="http://github.com/asleepwalker/typographie/" nofollow>Typographie на GitHub</a>.</p>
 			<h2>Структура репозитория</h2>
+			<b><a href="http://github.com/asleepwalker/typographie/tree/master/dist" nofollow>dist</a></b> — Последняя версия Typographie.<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/dist/typographie.class.php" nofollow>typographie.class.php</a></b> — Класс Typographie.<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/dist/example.php" nofollow>example.php</a></b> — Пример использования класса.<br>
 			<p><b><a href="http://github.com/asleepwalker/typographie/tree/master/api" nofollow>api</a></b> — Исходники api.typographie.ru<br>
-			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/api/index.php" nofollow>index.php</a></b> — Главный скрипт API<br>
-			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/api/rules.json" nofollow>rules.json</a></b> — Правила, которыми руководствуется API<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/api/index.php" nofollow>index.php</a></b> — Главный скрипт API.<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/api/rules.json" nofollow>rules.json</a></b> — Правила, которыми руководствуется API.<br>
 			<b><a href="http://github.com/asleepwalker/typographie/tree/master/src" nofollow>src</a></b> — Исходники typographie.ru<br>
 			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/tree/master/src/engine" nofollow>engine</a></b> — Серверные скрипты сервиса.<br>
 			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/engine/finediff.class.php" nofollow>finediff.class.php</a></b> — Модифицированный под UTF-8 <a href="http://github.com/gorhill/PHP-FineDiff" nofollow>PHP-FineDiff</a> для подсветки изменений.<br>
@@ -17,10 +20,9 @@
 			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/tree/master/src/pages" nofollow>pages</a></b> — Страницы сайта.<br>
 			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/index.php" nofollow>index.php</a></b> — Роутер страниц сайта.<br>
 			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/style.css" nofollow>style.css</a></b> — Общая каскадная таблица стилей.<br>
-			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/typographie.class.php" nofollow>typographie.class.php</a></b> — Ядро, копия оного в корне репозитория.<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/typographie.class.php" nofollow>typographie.class.php</a></b> — Класс Typographie.<br>
 			<b><a href="http://github.com/asleepwalker/typographie/blob/master/LICENSE" nofollow>LICENSE</a></b> — Лицензия MIT об условиях распространения и использования.<br>
-			<b><a href="http://github.com/asleepwalker/typographie/blob/master/README.md" nofollow>README.md</a></b> — Readme-файл для GitHub.<br>
-			<b><a href="http://github.com/asleepwalker/typographie/blob/master/typographie.class.php" nofollow>typographie.class.php</a></b> — Последняя версия ядра Typographie.</p>
+			<b><a href="http://github.com/asleepwalker/typographie/blob/master/README.md" nofollow>README.md</a></b> — Readme-файл для GitHub.</p>
 			<h2>Участие в проекте</h2>
 			<p>Активно приветствуется. :) Для того, чтобы предложить изменение/дополнение — выполните <a href="http://github.com/asleepwalker/typographie/pulls" nofollow>pull-request</a> на GitHub.</p>
 		</article>

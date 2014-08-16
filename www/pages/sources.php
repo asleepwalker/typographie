2ac745f51d755266e8f44e5edbfd3f33793ae7d4
Sleepwalker <mail@asleepwalker.ru>
2014-08-16 15:18:52 +0300

    Updated help pages

--- www/pages/sources.php
+++ www/pages/sources.php
@@ -11,16 +11,19 @@
 			<p><b><a href="http://github.com/asleepwalker/typographie/tree/master/api" nofollow>api</a></b> — Исходники api.typographie.ru<br>
 			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/api/index.php" nofollow>index.php</a></b> — Главный скрипт API.<br>
 			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/api/rules.json" nofollow>rules.json</a></b> — Правила, которыми руководствуется API.<br>
-			<b><a href="http://github.com/asleepwalker/typographie/tree/master/src" nofollow>src</a></b> — Исходники typographie.ru<br>
-			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/tree/master/src/engine" nofollow>engine</a></b> — Серверные скрипты сервиса.<br>
-			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/engine/finediff.class.php" nofollow>finediff.class.php</a></b> — Модифицированный под UTF-8 <a href="http://github.com/gorhill/PHP-FineDiff" nofollow>PHP-FineDiff</a> для подсветки изменений.<br>
-			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/engine/main.php" nofollow>main.php</a></b> — Менеджер, обрабатывающий AJAX-запросы сервиса.<br>
-			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/tree/master/src/i" nofollow>i</a></b> — Графика сайта.<br>
-			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/js/editor.js" nofollow>js/editor.js</a></b> — Обработчик событий, связывающий сервис с ядром.<br>
-			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/tree/master/src/pages" nofollow>pages</a></b> — Страницы сайта.<br>
-			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/index.php" nofollow>index.php</a></b> — Роутер страниц сайта.<br>
-			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/style.css" nofollow>style.css</a></b> — Общая каскадная таблица стилей.<br>
-			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/src/typographie.class.php" nofollow>typographie.class.php</a></b> — Класс Typographie.<br>
+			<b><a href="http://github.com/asleepwalker/typographie/tree/master/www" nofollow>www</a></b> — Исходники typographie.ru<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/tree/master/www/engine" nofollow>engine</a></b> — Серверные скрипты сервиса.<br>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/www/engine/main.php" nofollow>main.php</a></b> — Менеджер, обрабатывающий AJAX-запросы сервиса.<br>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/www/engine/typographie.class.php" nofollow>typographie.class.php</a></b> — Backend-ядро сервиса.<br>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/www/engine/converter.class.php" nofollow>converter.class.php</a></b> — Расширение ядра для конвертирования.<br>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/www/engine/finediff.class.php" nofollow>finediff.class.php</a></b> — Модифицированный под UTF-8 <a href="http://github.com/gorhill/PHP-FineDiff" nofollow>PHP-FineDiff</a> для подсветки изменений.<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/tree/master/www/i" nofollow>i</a></b> — Графика сайта.<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/tree/master/www/js" nofollow>js</a></b> — Скрипты сайта.<br>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/www/js/editor.js" nofollow>js/app.js</a></b> — Frontend-ядро сервиса.<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/tree/master/www/pages" nofollow>pages</a></b> — Страницы сайта.<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/www/index.php" nofollow>index.php</a></b> — Роутер страниц сайта.<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/www/style.css" nofollow>style.css</a></b> — Общая каскадная таблица стилей.<br>
+			&nbsp;&nbsp;&nbsp;<b><a href="http://github.com/asleepwalker/typographie/blob/master/www/defaults.json" nofollow>defaults.json</a></b> — Конфигурация сервиса.<br>
 			<b><a href="http://github.com/asleepwalker/typographie/blob/master/LICENSE" nofollow>LICENSE</a></b> — Лицензия MIT об условиях распространения и использования.<br>
 			<b><a href="http://github.com/asleepwalker/typographie/blob/master/README.md" nofollow>README.md</a></b> — Readme-файл для GitHub.</p>
 			<h2>Участие в проекте</h2>

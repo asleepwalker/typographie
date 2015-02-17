b35a1a937064a1272c26dc20a79bdc9b08f8ffa4
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2015-02-18 01:38:34 +0200

    Updated project years

--- www/pages/footer.php
+++ www/pages/footer.php
@@ -5,7 +5,7 @@
 				<li><a href="?about">О сервисе</a></li>
 				<li><a href="?api">API</a></li>
 				<li><a href="?sources">Исходники</a></li>
-				<li id="author"><a href="http://me.asleepwalker.ru/"><span class="icon">Sleepwalker</span></a>, 2014</li>
+				<li id="author"><a href="http://me.asleepwalker.ru/"><span class="icon">Sleepwalker</span></a>, 2014–<?= date('Y') ?></li>
 			</ul></nav>
 		</footer>
 	</body>

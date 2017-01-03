aca63e12cc558c05f0fbf6590d7d06b5e5695b42
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-01-03 14:07:13 +0200

    Update contacts

--- www/pages/contacts.php
+++ www/pages/contacts.php
@@ -5,11 +5,10 @@
 			<p>Меня зовут <b>Артём Федосов</b>, в онлайне известен как <b>Sleepwalker</b>. Я независимый веб-разработчик, пишу сервисы, рисую дизайн, вношу вклад в open-source.</p>
 			<p>Связаться со мной можно любым из следующих вариантов:<br>
 			&nbsp;&nbsp;Skype: <b>nsleepwalker</b><br>
-			&nbsp;&nbsp;Мобильный: <b>+38 063 463‒70‒86</b><br>
+			&nbsp;&nbsp;Мобильный: <b>+38 067 089‒93‒32</b><br>
 			&nbsp;&nbsp;E-mail: <a href="mailto:mail@asleepwalker.ru" nofollow>mail@asleepwalker.ru</a><br>
 			&nbsp;&nbsp;Соцсети: <a href="http://vk.com/nsleepwalker" nofollow>VK</a>, <a href="http://www.facebook.com/asleepwalker" nofollow>Facebook</a>, <a href="http://twitter.com/usleepwalker" nofollow>Twitter</a>, <a href="http://plus.google.com/116995323828725895922/posts" nofollow>Google+</a>.</p>
 			<p>Также, если вопрос касается непосредственно сервиса, вы можете написать на GitHub <a href="http://github.com/asleepwalker/typographie/issues" nofollow>в раздел issues</a>.</p>
-			<p>P. S. Открыт к предложениям сотрудничества.</p>
 		</article>
 	</div>
 </div>

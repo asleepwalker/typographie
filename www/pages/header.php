70baee7241433966ffafb398951fd409899b390f
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-02-03 21:39:20 +0200

    Add meta tags

--- www/pages/header.php
+++ www/pages/header.php
@@ -3,6 +3,18 @@
 	<head>
 		<title><?php if (isset($title)) echo $title.' — '; ?>Typographie</title>
 		<meta charset="utf-8">
+		<link rel="icon" href="/favicon.ico">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Сервис «Typographie» автоматизирует подготовку текстов к веб-публикации. Вам больше не понадобится запоминать alt-коды и заботиться о неразрывных пробелах: вы пишете текст в привычном для себя формате, получаете — готовый к публикации.">
+		<meta name="keywords" content="типограф, типографика, спецсимволы, подготовка, публикация, typographie">
+		<meta property="og:title" content="<?= isset($title) ? $title.' — Typographie' : 'Typographie — подготовка текстов к веб-публикации онлайн' ?>">
+		<meta property="og:description" content="Сервис «Typographie» автоматизирует подготовку текстов к веб-публикации. Вам больше не понадобится запоминать alt-коды и заботиться о неразрывных пробелах: вы пишете текст в привычном для себя формате, получаете — готовый к публикации.">
+		<meta property="og:image" content="http://typographie.ru/i/logo.png">
+		<meta property="og:locale" content="ru_RU">
+		<meta property="og:type" content="website">
+		<meta property="og:site_name" content="Typographie">
+		<meta property="og:url" content="http://typographie.ru<?= $_SERVER[REQUEST_URI] ?>">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<!--[if IE]>
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
